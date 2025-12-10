7a539b561d55f4ad1f2c4f23cdd34d890851477e
jackson-evarts <jack.evarts@gmail.com>
2025-12-09 21:03:59 -0800

    Initial Commit

---
+++ Genius Square Evaluator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Genius Square Evaluator
+//
+//  Created by Jackson Evarts on 12/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Genius Square Evaluator/Genius_Square_EvaluatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Genius_Square_EvaluatorApp.swift
+//  Genius Square Evaluator
+//
+//  Created by Jackson Evarts on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Genius_Square_EvaluatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
